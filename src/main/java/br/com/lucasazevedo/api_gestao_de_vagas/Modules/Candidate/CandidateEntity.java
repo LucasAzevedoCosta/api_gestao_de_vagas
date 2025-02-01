4b3128f8edb9fd7aa22b7f78da4a0321b7820374
Lucas Azevedo Costa <eozecos@gmail.com>
2025-02-01 19:44:38 -0300

    Criando tabela de candidato

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/CandidateEntity.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/CandidateEntity.java
@@ -1,16 +1,25 @@
 package br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "Candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -23,6 +32,9 @@ public class CandidateEntity {
     @Length(min = 8, max= 20, message = "A senha deve ter no mínimo 8 caracteres") 
     private String password;
     private String descripition;
-    private String curriculum;    
+    private String curriculum;
+
+    @CreationTimestamp()
+    private LocalDateTime createdAt;
 
 }

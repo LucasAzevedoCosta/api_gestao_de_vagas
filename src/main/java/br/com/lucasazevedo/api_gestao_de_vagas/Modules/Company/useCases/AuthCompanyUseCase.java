11609c8b994812d78ba88d170326e8fb77b22ae0
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-06 00:31:17 -0300

    Recuperando JWT

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.lucasazevedo.api_gestao_de_vagas.Modules.Company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +45,9 @@ public class AuthCompanyUseCase {
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         var token = JWT.create().withIssuer("Lucas.inc")
-            .withSubject(company.getId().toString()).sign(algorithm);
+            .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+            .withSubject(company.getId().toString())
+            .sign(algorithm);
 
         return token;
     }

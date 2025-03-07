ece348375ded04de47e9e57607f07fe6bda7ff8c
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-07 10:26:33 -0300

    Criando Rota de perfil do candidato

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/Controllers/CandidateController.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/Controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.Controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.CandidateEntity;
 import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.useCases.CreateCandidateUseCase;
+import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,11 +23,27 @@ public class CandidateController {
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
+
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity CandidateEntity ) {
-        try{
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity CandidateEntity) {
+        try {
             var result = this.createCandidateUseCase.execute(CandidateEntity);
             return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+        var idCandidate = request.getAttribute("candidate_id");
+
+        try {
+            var profile = this.profileCandidateUseCase
+                    .execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }

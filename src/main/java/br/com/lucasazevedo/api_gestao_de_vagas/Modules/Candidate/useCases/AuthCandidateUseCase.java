bf912a52fa8f73f32300df05ad8a6dcffd92b095
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-08 00:27:08 -0300

    Configurando PreAuthorize

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/Modules/Candidate/useCases/AuthCandidateUseCase.java
@@ -51,7 +51,7 @@ public class AuthCandidateUseCase {
         var token = JWT.create()
                 .withIssuer("Lucas.inc")
                 .withSubject(candidate.getId().toString())
-                .withClaim("roles", Arrays.asList("candidate"))
+                .withClaim("roles", Arrays.asList("CANDIDATE"))
                 .withExpiresAt(expiresIn)
                 .sign(algorithm);
 

61d7807a62c24a02b256740b3f4a7a6ba5f095d7
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-09 22:06:34 -0300

    Configurando o Swagger

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/ApiGestaoDeVagasApplication.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/ApiGestaoDeVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.lucasazevedo.api_gestao_de_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável por gerenciar vagas de emprego", version = "1"))
 public class ApiGestaoDeVagasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/security/SecurityConfig.java
@@ -9,36 +9,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-
 @Configuration
 public class SecurityConfig {
 
-  @Autowired
-  private SecurityFilter securityFilter;
+    @Autowired
+    private SecurityFilter securityFilter;
 
-  @Autowired
-  private SecurityCandidateFilter securityCandidateFilter;
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
+    private static final String[] SWAGGER_LIST = {
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-resource/**",};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> {
-          auth.requestMatchers("/candidate/").permitAll()
-              .requestMatchers("/company/").permitAll()
-              .requestMatchers("/company/auth").permitAll()
-              .requestMatchers("/candidate/auth").permitAll();
-              auth.anyRequest().authenticated();
-        }).addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
-        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
-        
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll()
+                            .requestMatchers("/company/auth").permitAll()
+                            .requestMatchers("/candidate/auth").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll();
+                    auth.anyRequest().authenticated();
+                }).addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
 }

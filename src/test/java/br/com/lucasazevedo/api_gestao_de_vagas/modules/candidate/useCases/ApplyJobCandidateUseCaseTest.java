8b44997cab57d75274487987babefeb992004ccc
Lucas Azevedo Costa <eozecos@gmail.com>
2025-03-17 21:27:44 -0300

    :test_tube: Test: validando se o Job não existe

--- src/test/java/br/com/lucasazevedo/api_gestao_de_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/lucasazevedo/api_gestao_de_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,16 +1,22 @@
 package br.com.lucasazevedo.api_gestao_de_vagas.modules.candidate.useCases;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.CandidateEntity;
 import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.CandidateRepository;
 import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Candidate.useCases.ApplyJobCandidateUseCase;
 import br.com.lucasazevedo.api_gestao_de_vagas.Modules.Company.repositories.JobRepository;
+import br.com.lucasazevedo.api_gestao_de_vagas.exceptions.JobNotFoundException;
 import br.com.lucasazevedo.api_gestao_de_vagas.exceptions.UserNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,6 +38,22 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+        
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try {
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }

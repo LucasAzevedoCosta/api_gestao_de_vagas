66f032e8e657b607542c31f8657523a726cddbe9
LucasAzevedoCosta <eozecos@gmail.com>
2025-04-20 16:37:53 -0300

    :bricks: Config: Configurando o .env

--- src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/ApiGestaoDeVagasApplication.java
+++ src/main/java/br/com/lucasazevedo/api_gestao_de_vagas/ApiGestaoDeVagasApplication.java
@@ -3,10 +3,19 @@ package br.com.lucasazevedo.api_gestao_de_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiGestaoDeVagasApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		
 		SpringApplication.run(ApiGestaoDeVagasApplication.class, args);
 	}
 
